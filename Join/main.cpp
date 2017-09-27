aad03f44f6d5aebe00ec382252dacf58ce72c7c4
hikarido <tocin723@gmail.com>
2017-09-27 19:05:04 +0000

    init dr.Web test task 	new file:   Join/CMakeLists.txt 	new file:   Join/main.cpp

---
+++ Join/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+Тестовое задание:
+Необходимо реализовать функцию.
+
+Прототип функции:
+void join (const std::vector< int > &from, std::vector< int > &to);
+
+Она должна принимать два отсортированных вектора (отсортированность
+принимается на веру - проверять не нужно, порядок - по возрастанию).
+На выходе вектор from не изменяется, а вектор to содержит все элементы
+из обоих, так же расположенных в порядке возрастания.
+*/
+
+void join(const std::vector<int> &from, std::vector<int> &to);
+
+int main()
+{
+
+    return 0;
+}
+
