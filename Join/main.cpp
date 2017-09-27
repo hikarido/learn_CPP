63e4aae0349d3f8011905b6d6313d7bd2258a4af
hikarido <tocin723@gmail.com>
2017-09-27 20:13:28 +0000

    first approximation 	modified:   Join/CMakeLists.txt 	modified:   Join/main.cpp

--- Join/main.cpp
+++ Join/main.cpp
@@ -17,10 +17,46 @@ void join (const std::vector< int > &from, std::vector< int > &to);
 из обоих, так же расположенных в порядке возрастания.
 */
 
-void join(const std::vector<int> &from, std::vector<int> &to);
+
+void print(auto &vec)
+{
+    cout<<"[";
+    for(auto &i: vec)
+    {
+        cout << i <<" ";
+    }
+    cout<<"]"<<endl;
+}
+
+
+void join(const std::vector<int> &from, std::vector<int> &to)
+{
+    vector<int> res(to.size()+from.size());
+    merge(from.begin(), from.end(),
+         to.begin(), to.end(), res.begin());
+    to = move(res);
+}
 
 int main()
 {
+    vector<int> a = {1,2,3, 1100, 2344};
+    vector<int> b = {23,24,25};
+
+
+    cout<<"before"<<endl;
+    print(a);
+    print(b);
+    join(b, a);
+
+    cout<<"after"<<endl;
+    print(a);
+    print(b);
+
+
+
+    cout << endl;
+
+
 
     return 0;
 }
