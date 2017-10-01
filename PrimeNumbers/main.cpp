8deccc7bdc8a24f8c1e82ba014a9b5eda9dd8d3b
hikarido <tocin723@gmail.com>
2017-10-01 07:46:08 +0000

    start coding primal numbers finder

---
+++ PrimeNumbers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief main
+ * Destination of this program is calculation prime numbers
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[])
+{
+    cout << "Prime Numbers start" <<endl;
+    return 0;
+}
+
