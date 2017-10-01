0e2d1b179f78f324d8a8404f88952dafe32f275c
hikarido <tocin723@gmail.com>
2017-10-01 07:54:07 +0000

    added working modes and start parce function to it

--- PrimeNumbers/main.cpp
+++ PrimeNumbers/main.cpp
@@ -3,6 +3,33 @@
 using namespace std;
 
 /**
+ * @brief The Mod enum
+ * working mode of programm
+ */
+enum class Mod
+{
+    help,       // print help
+    bench_mark, // detect and display work time
+    normal,     // just find and display numbers
+    miss_arg    // notify about missing argv
+};
+
+/**
+ * @brief parce_argv
+ * parce argv according with Mode deskription
+ * @param argc
+ * @param argv
+ * @return Working mode
+ */
+Mod parce_argv(int argc, char *argv[])
+{
+
+    return Mod::normal;
+}
+
+
+
+/**
  * @brief main
  * Destination of this program is calculation prime numbers
  * @param argc
