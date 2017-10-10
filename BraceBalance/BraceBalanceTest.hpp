be6a9650475ee71b346eabf46b8d9549ad3767ba
hikarido <tocin723@gmail.com>
2017-10-10 14:46:11 +0000

    added skilet of Test class

--- BraceBalance/BraceBalanceTest.hpp
+++ BraceBalance/BraceBalanceTest.hpp
@@ -1,9 +1,30 @@
 #ifndef BRACE_BALANCE_TEST_HPP
 #define BRACE_BALANCE_TEST_HPP
+
+#include<iostream>
+#include <cassert>
 #include"BraceBalance.hpp"
 
-class BraceBalanceTest{
+using std::cout;
+using std::endl;
 
+
+class BraceBalanceTest
+{
+public:
+    BraceBalanceTest()
+    {
+        cout << "test 1 passing" <<endl;
+        test_1();
+    }
+
+    ~BraceBalanceTest() = default;
+
+    void test_1()
+    {
+
+
+    }
 };
 
 #endif
