8e0e55a0e5dd5468017353413bea677f8d860ac6
hikarido <tocin723@gmail.com>
2017-10-10 14:41:17 +0000

    added skilet of Balance class

--- BraceBalance/BraceBalance.hpp
+++ BraceBalance/BraceBalance.hpp
@@ -1,6 +1,23 @@
 #ifndef BRACE_BALANCE_HPP
 #define BRACE_BALANCE_HPP
+
+#include <stack>
+#include <string>
+
 class BraceBalance{
+
+public:
+    BraceBalance() = default;
+    ~BraceBalance() = default;
+    bool check(const std::string & sentence);
+
+private:
+    bool open_close_correct(char open, char close);
+    bool is_open(char brace);
+    bool is_close(char brace);
+
+    std::stack<char> store;
+
 
 };
 
