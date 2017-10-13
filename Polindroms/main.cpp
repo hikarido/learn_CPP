f1469d7069ed579d2b086619ea6ec001b8b2721e
hikarido <tocin723@gmail.com>
2017-10-13 20:16:26 +0000

    Start write programm about polindroms 	new file:   Polindroms/CMakeLists.txt 	new file:   Polindroms/main.cpp

---
+++ Polindroms/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief is_word_polindrom
+ * Determin what is the word polindrom or nof
+ *
+ * @details
+ * EXAMPLE
+ * =======
+ * not polindroms: "hello","123jji321", "keks"
+ * polindroms: "223heh322", "kak", " ", "", "a"
+ * @pram word supposed polindrom
+ * @return true if word is polindrom
+ */
+bool is_word_polindrom(const string & word);
+
+/**
+ * @brief is_word_polindrom_test
+ * Perform testing is_word_polindrom()
+ */
+void is_word_polindrom_test();
+
+/**
+ * @brief main
+ * Polindrom test area
+ * TODO check some book on polindroms
+ * @return
+ */
+int main()
+{
+    return 0;
+}
+
