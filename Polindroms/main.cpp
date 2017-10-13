f4a9aadca5f3f585327b163d0f3a63731d120023
hikarido <tocin723@gmail.com>
2017-10-13 20:44:05 +0000

    first approximation done. On one of tests test not perform correct. 	modified:   Polindroms/main.cpp

--- Polindroms/main.cpp
+++ Polindroms/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 
 using namespace std;
 
@@ -10,18 +11,63 @@ using namespace std;
  * @details
  * EXAMPLE
  * =======
- * not polindroms: "hello","123jji321", "keks"
- * polindroms: "223heh322", "kak", " ", "", "a"
+ * not polindroms: "hello","123jji321", "keks", " ", "", "a"
+ * polindroms: "223heh322", "kak"
  * @pram word supposed polindrom
  * @return true if word is polindrom
  */
-bool is_word_polindrom(const string & word);
+bool is_word_polindrom(const string & word)
+{
+    // ""
+    if(word.empty())
+        return false;
+
+    // one  character
+    if(word.size() == 1)
+        return false;
+
+    auto first = word.cbegin();
+    auto second = word.cend() - 1;
+
+    while(first <= second)
+    {
+        if(*first != *second)
+            return false;
+
+        ++first;
+        --second;
+    }
+
+    return true;
+}
 
 /**
  * @brief is_word_polindrom_test
  * Perform testing is_word_polindrom()
  */
-void is_word_polindrom_test();
+void is_word_polindrom_test()
+{
+    assert(is_word_polindrom("") == false);
+    assert(is_word_polindrom(" ") == false);
+    assert(is_word_polindrom("a") == false);
+    string chars = "`1234567890-=\\qwertyuiop[]asdfghjkl;/'zxcvbnm,.\
+            QWERTYUIOPLKJHGFDSAZXCVBNM";
+
+    for(char c: chars)
+            assert(is_word_polindrom(string(1, c)) == false);
+
+    assert(is_word_polindrom("aa") == true);
+    assert(is_word_polindrom("aaa") == true);
+    assert(is_word_polindrom("aaaa") == true);
+    assert(is_word_polindrom("aaaaa") == true);
+    assert(is_word_polindrom("aaaaaa") == true);
+    assert(is_word_polindrom("aba") == true);
+    assert(is_word_polindrom("abba") == true);
+    assert(is_word_polindrom("abcba") == true);
+    assert(is_word_polindrom("-1\/1-") == true);
+//    assert(is_word_polindrom("-1\|/1-") == true); <-- here faild
+
+}
 
 /**
  * @brief main
@@ -31,6 +77,8 @@ void is_word_polindrom_test();
  */
 int main()
 {
+    cout << "is_word_polindrom_test(); pessing" << endl;
+    is_word_polindrom_test();
     return 0;
 }
 
