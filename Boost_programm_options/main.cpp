1c8812cacfb4c2736f5325ccf18d729d6508f748
hikarido <tocin723@gmail.com>
2018-06-19 00:06:14 +0300

    boost loging and arg parcer test 	modified:   ../CMakeLists.txt 	modified:   ../main.cpp

--- Boost_programm_options/main.cpp
+++ Boost_programm_options/main.cpp
@@ -4,18 +4,35 @@
 
 
 #include "boost/log/trivial.hpp"
-
+#include "boost/program_options.hpp"
+#include <vector>
 using namespace boost::log::trivial;
+namespace po = boost::program_options;
 
-int main()
+int main(int argc, char *argv[])
 {
+	try{
+		po::options_description desc("This prog");
+		desc.add_options()
+				("help", "Can I to help you")
+				("compression", po::value<int>(), "set compression level");
 
-	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-	BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-	BOOST_LOG_TRIVIAL(error) << "An error severity message";
-	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
 
-	return 0;
+		if (vm.count("compression")) {
+			BOOST_LOG_TRIVIAL(info) << "compression arg passed";
+			return 1;
+		} else if (vm.count("help")) {
+			BOOST_LOG_TRIVIAL(info) << desc;
+			return 2;
+		}
+
+		return 0;
+
+	}
+	catch(std::exception &e){
+		BOOST_LOG_TRIVIAL(error) << "Error when parse cmb args: " << e.what();
+		return -1;
+	}
 }
