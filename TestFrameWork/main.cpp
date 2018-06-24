7d6274ac3ec6911d7f787022b62e01a9793816c1
hikarido <tocin723@gmail.com>
2018-06-25 00:45:49 +0300

    simplest test for simple framework 	modified:   main.cpp 	modified:   simpleTestFramework.h

--- TestFrameWork/main.cpp
+++ TestFrameWork/main.cpp
@@ -4,13 +4,26 @@
 
 #include "simpleTestFramework.h"
 
-int main(){
+void test_1(){
+	Assert(1 == 2, "lustig test 1");
+}
+
+void test_map_equals(){
 	std::map<pair<int, int>, vector<int>> m = {{{1,2}, {1,2,3}}};
 	decltype(m) ans_m = {{{1,2}, {1,3,5}}};
-	AssertEqual(m, ans_m);
+	AssertEqual(m, ans_m, "data structures in test");
+}
 
-	int a = 3;
-	int b = 4;
-	AssertEqual(3,4, "test 1");
+void correct_test(){
+	Assert(true, "yes that is");
+}
+
+int main(){
+	RunTest(test_1, "test_1");
+	RunTest(test_map_equals, "test_map_equals");
+	RunTest(correct_test, "correct_test");
+//	int a = 3;
+//	int b = 4;
+//	AssertEqual(3,4, "test 1");
 	return 0;
 }
--- TestFrameWork/simpleTestFramework.h
+++ TestFrameWork/simpleTestFramework.h
@@ -1,6 +1,11 @@
 //
 // Created by hikarido on 6/24/18.
+// Простой тестовый фреймворк
+// Использование
 //
+//
+//
+
 
 #ifndef ALLTOSQUARE_SIMPLETESTFRAMEWORK_H
 #define ALLTOSQUARE_SIMPLETESTFRAMEWORK_H
@@ -8,6 +13,7 @@
 #include <sstream>
 #include <exception>
 #include <string>
+#include <ostream>
 
 #include "printFramework.h"
 
@@ -15,23 +21,65 @@ using std::ostringstream;
 using std::runtime_error;
 using std::string;
 using std::endl;
+using std::cout;
+
+const string pass_correct = "PASS";
+const string pass_failed = "FAILED";
 
 /**
  * Проверка на эквивалентность двух выражений
+ * Ожидается, что expect == exist, если это не  так то выполняется оповещение о том, то
+ * ожидание расходится с реальностью
  * @tparam Expect must have oprator !=
  * @tparam Exist  must have operator !=
  * @param expect значение ожидаемое
  * @param exist значение предоставленное, в сравнение к ожижаемому
- * @param hint std::string unique description for more detalisation, default = NONE
+ * @param hint std::string unique description for more detalisation, default = "NONE"
+ *
+ * Бросает исключение std::runtime_error если тест выстрелил
  */
 template<typename Expect, typename Exist>
 void AssertEqual(const Expect & expect, const Exist & exist, const string & hint = "NONE"){
 	if(expect != exist){
 		ostringstream ost;
-		ost << "Assertation failed: " << expect << " != " << exist << " HINT: " << hint<< endl;
+		ost << " "<< expect << " != " << exist << " HINT: " << hint<< endl;
 		throw runtime_error(ost.str());
 	}
 };
+
+/**
+ * Замена для стандартного assert
+ * ожидается что expect_true == true
+ * если это не так то возбуждается исключение и отрабатывает вывод инфррмации
+ * о расхождении с ожидаемым результатом
+ * @param expect_true bool результат выполнения какого либо выражения
+ * @param hint string уникальное название теста выводится в блоке HINT, default = "NONE"
+ *
+ * Бросае исключение std::runtime_error если тест выстрелил
+ */
+void Assert(bool expect_true, const string & hint = "NONE"){
+	AssertEqual(expect_true, true, hint);
+}
+
+/**
+ * Среда для запуска тестовых функций
+ * Ловит исключения при выстреле тестов и позволяет выполнить все тесты
+ * Что бы определить совокупность рабочего и не рабочего кода согласно тестам
+ * @tparam FunctionAgregatedTestFunction некая функция которая в себе исполняет одну и более тестовых функций
+ * @param test тестовая функци
+ * @param test_name std::string имя теста
+ */
+template<typename FunctionAgregatedTestFunction>
+void RunTest(FunctionAgregatedTestFunction test, const string & test_name){
+	try{
+		test();
+		cout << "Test " << test_name << " " << pass_correct << endl;
+	} catch(runtime_error & test_exception){
+		cout << "Test " << test_name << " " << pass_failed << " " << test_exception.what();
+	}
+}
+
+
 
 
 #endif //ALLTOSQUARE_SIMPLETESTFRAMEWORK_H
